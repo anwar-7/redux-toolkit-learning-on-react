001e4978a777ac7e6ffb0e23d7497a6f8b2b3e9c
anwarul1 <anwarjewel007@gmail.com>
2022-09-14 17:56:48 +0600

    redux-toolkit rerning on react project and make a counter app

---
+++ src/components/Counter.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  decrementCounter,
+  incrementCounter,
+  resetCounter,
+} from '../services/actions/counterAction';
+
+const Counter = () => {
+  const count = useSelector((state) => state.count);
+  // console.log(count);
+  //
+  const dispatch = useDispatch();
+  // console.log(dispatch);
+
+  const handleIncrement = () => {
+    dispatch(incrementCounter());
+    //
+  };
+  const handleReset = () => {
+    dispatch(resetCounter());
+    //
+  };
+  const handleDecrement = () => {
+    dispatch(decrementCounter());
+    //
+  };
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+
+      <h3>Count: {count}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
+
+// 1. state - count: 0
+// 2. action - increment, decrement, reset
+// 3. reducers - increment -> count => count + 1
+// - decrement -> count => count - 1
+// - reset -> -> count => 0
+// 4. store
+// 5. providing store in react
+// 6. use store
